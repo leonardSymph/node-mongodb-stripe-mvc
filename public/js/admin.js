d6f0f9da6b121d5f0cdb465358d3ee9647d398b3
leonardSymph <leonard@symph.co>
2021-08-02 01:52:15 +0800

    re-update

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,25 +1,32 @@
-console.log('testing')
+console.log("testing");
 
-const deleteProduct =(buttonElement)=>{
-    const prodId = buttonElement.parentNode.querySelector('input[name=productId]').value
-    const csrfToken = buttonElement.parentNode.querySelector('input[name=_csrf]').value
+const deleteProduct = (buttonElement) => {
+  const prodId = buttonElement.parentNode.querySelector(
+    "input[name=productId]"
+  ).value;
+  const csrfToken =
+    buttonElement.parentNode.querySelector("input[name=_csrf]").value;
 
-    // the product
-    const productElement = buttonElement.closest('article')
+  console.log("try-delete");
 
-    console.log(prodId, csrfToken)
-    fetch("/admin/products/" + prodId,{
-        method: 'DELETE',
-        headers: {
-            'csrf-token': csrfToken
-        }
+  // the product
+  const productElement = buttonElement.closest("article");
+
+  console.log(prodId, csrfToken);
+  fetch("/admin/products/" + prodId, {
+    method: "DELETE",
+    headers: {
+      "csrf-token": csrfToken,
+    },
+  })
+    .then((result) => {
+      return result.json();
     })
-    .then(result=>{
-        return result.json()
+    .then((data) => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement);
     })
-    .then(data=>{
-        console.log(data);
-        productElement.parentNode.removeChild(productElement)
-    })
-    .catch((err)=>{console.log(err)})
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
